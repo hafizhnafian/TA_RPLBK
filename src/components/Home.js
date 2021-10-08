9a2f10c1a90163d9394e50fb74cdb0c75628f05a
Hafizh Nafian <hafizhnafian@gmail.com>
2021-10-08 13:20:39 +0700

    TA RPLBK

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,10 @@ export default class Home extends Component {
     render() {
         return(
             <div>
-            <p className="text-center text-dark-600 font-normal">Selamat Datang di Sistem Informasi Kelompok 49</p>
+                <h2 className="text-3xl font-extrabold trackingtight text-indigo-900 sm:text-4xl text-center">
+                    <p>Selamat Datang di Sistem Informasi</p>
+                    <p>Kelompok 49</p>
+                </h2>
             </div>
         );
     }
