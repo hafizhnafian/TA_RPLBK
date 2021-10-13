54661cdd399faf2644ca790187a4943abc20798d
Dessinta Rahmadani <50995379+dessintadr@users.noreply.github.com>
2021-10-13 14:34:52 +0700

    Update Mahasiswa.js

--- src/components/Mahasiswa.js
+++ src/components/Mahasiswa.js
@@ -1,45 +1,99 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useContext, createContext } from "react";
 import axios from "axios";
-import Card from "./Card";
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import { Grid, Modal, Box } from "@mui/material";
 
-export default class Mahasiswa extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            Mahasiswa: [],
-            visible: false,
-        };
-    }
-    handleButton = (alamat) => {
-        alert("Alamat : " + alamat);
-    };
-    componentDidMount() {
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 800,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+const DetailMahasiswa = createContext();
+
+function Mahasiswa(props) {
+    const [mahasiswa, setMahasiswa] = useState([]);
+    const [detail, setDetail] = useState([]);
+    console.log(detail);
+    const [open, setOpen] = useState(false);
+    const handleClose = () => setOpen(false);
+
+    useEffect(() => {
         axios({
             method: "get",
-            url: "    http://localhost:3000/mahasiswa ",
+            url: "http://localhost:3000/mahasiswa",
             headers: {
                 accept: "*/*",
             },
         })
+
             .then((data) => {
-                console.log(data.data);
-                this.setState({
-                    Mahasiswa: data.data,
-                });
+                setMahasiswa(data.data);
+
             })
             .catch((error) => {
                 console.log(error);
-            });
-    }
-    render() {
-        return (
-            <div className="flex flex-row flex-wrap mt-2 justify-start">
-                    {this.state.Mahasiswa.map((results, index) => {
-                        return (
-                            <Card name={results.name} nim={results.nim} image={results.image}/>
-                        );
-                    })}
-            </div>
-        );
-    }
+            })
+
+    }, [])
+    return (
+        <Container>
+
+            <Grid>
+                {mahasiswa.map((result) => {
+                    return (
+                        <Grid>
+                            <Card>
+                                <CardActionArea onClick={() => { setOpen(true); setDetail([result.address, result.tlp]) }}>
+                                    <CardMedia
+                                        component="img"
+                                        height="200"
+                                        image={result.image}
+                                    />
+                                </CardActionArea>
+                                <CardContent>
+
+                                    <Typography variant="h5">{result.name}</Typography>
+                                    <Typography variant="body2">{result.nim}​​​​​​</Typography>
+
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        {detail[0]}
+
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        {detail[1]}
+                    </Typography>
+                </Box>
+            </Modal>
+        </Container>
+
+
+    );
 }
+
+export default Mahasiswa;
